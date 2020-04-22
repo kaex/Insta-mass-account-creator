adccb46a2149c117aaa915256d896c222eff7ae0
xosad <root@baransel.dev>
2020-04-22 20:29:56 +0300

    + added the new headers + updated the requests

--- start.php
+++ start.php
@@ -15,14 +15,12 @@ $handle = fopen('php://stdin', 'rb');
 $line   = fgets($handle);
 if (trim($line) === '1')
 {
-	system('clear');
 	echo 'Generating Instagram Account!' . PHP_EOL;
 	$i = new igGenerator((int)getopt('l:h:p:h')['l'] ?: 1, getopt('l:h:p:h')['p'] ?: null);
 	$i->generateInstagramAccount();
 }
 else if (trim($line) === '2')
 {
-	system('clear');
 	echo 'What accounts should i follow? Seperate them with a comma (eg: username1, username2)' . PHP_EOL;
 	$handle = fopen('php://stdin', 'rb');
 	$line   = fgets($handle);
